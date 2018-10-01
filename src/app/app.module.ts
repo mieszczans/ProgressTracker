c40c899aeb5a3c9c0c5017f017c090d86452c6ce
sebastian mieszczanczyk <sebastianmieszczanczyk@gmail.com>
2018-10-01 14:41:46 +0200

    initialize project (add material and hammerjs)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
